bc065d388cec26f56d1e755441262e5434ea63c3
Phil Hawksworth <phil@netlify.com>
2021-07-21 17:53:54 +0100

    switch to GET

--- netlify/functions/ask.js
+++ netlify/functions/ask.js
@@ -4,23 +4,10 @@ const striptags = require('striptags');
 
 exports.handler = async (event) => {
 
-  console.log({event});
-  
-  
-  if (event.httpMethod !== "POST") {
-    return {
-      statusCode: 500,
-      body: "HTTP method not allowed"
-    };
-  } 
-
-
-  const { question } = querystring.parse(event.body);
+  const { question } = event.queryStringParameters;
   const plainQuestion = striptags(question);
   const path = slugify(plainQuestion);
   
-
-  console.log(question);
   return {
     statusCode: "302",
     headers: {
@@ -29,3 +16,4 @@ exports.handler = async (event) => {
   };
 
 };
+

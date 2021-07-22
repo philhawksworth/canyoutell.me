288546530d6d82005e37940e37d170d601b8539f
Phil Hawksworth <phil@netlify.com>
2021-07-22 10:56:37 +0100

    use POST to avoid qs prams always propagating

--- netlify/functions/ask.js
+++ netlify/functions/ask.js
@@ -1,14 +1,25 @@
 const slugify = require("slugify");
 const striptags = require('striptags');
+const querystring = require("querystring");
 
 exports.handler = async (event) => {
   
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
   console.log(event);
 
   try {
-    const { question } = event.queryStringParameters;
-    const plainQuestion = striptags(question);
+    const params = querystring.parse(event.body);
+    // const { question } = event.queryStringParameters;
+    const plainQuestion = striptags(params.question);
     const path = slugify(plainQuestion);
+
+    console.log(`path: ${path}`);
+    
+
     return {
       statusCode: 302,
       headers: {

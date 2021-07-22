586b3df4b16687ffae7007cce94909533c636432
Phil Hawksworth <phil@netlify.com>
2021-07-22 10:59:08 +0100

    clean up and refactor

--- netlify/functions/ask.js
+++ netlify/functions/ask.js
@@ -4,22 +4,14 @@ const querystring = require("querystring");
 
 exports.handler = async (event) => {
   
-  // Only allow POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  console.log(event);
-
   try {
     const params = querystring.parse(event.body);
-    // const { question } = event.queryStringParameters;
     const plainQuestion = striptags(params.question);
     const path = slugify(plainQuestion);
-
-    console.log(`path: ${path}`);
-    
-
     return {
       statusCode: 302,
       headers: {
